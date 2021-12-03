8bf2d6f6ddb86840a9d3a32e1beb3f274f19792c
Martins, Manuel Tarouca <manuemartins@deloitte.pt>
2021-12-03 18:18:18 +0000

    Rust is hard !

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+  let path = "./src/inputs.txt";
+  println!("Hello, world!");
+  let inputs: String = fs::read_to_string(path).expect("something went wrong with the file");
+  let inputs_arr: Vec<&str> = inputs.split("\n").collect();
+  for input in inputs_arr {
+    let chars: Vec<char> = input.chars().collect();
+    for c in chars {
+      println!("{}",c);
+    }
+    println!("Inputs:\n{}", input);
+  }
 }
